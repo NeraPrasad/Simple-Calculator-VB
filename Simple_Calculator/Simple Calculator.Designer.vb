afab07f91cdf817e94d9fc3c43122bdffec13002
NeraPrasad <49784919+NeraPrasad@users.noreply.github.com>
2019-06-08 04:53:48 -0700

    Simple-Calculator-VB

---
+++ Simple_Calculator/Simple Calculator.Designer.vb
@@ -0,0 +1,292 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDiviAnswer = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDivi2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.txtDivi1 = New System.Windows.Forms.TextBox()
+        Me.txtMultiAnswer = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtMulti2 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.txtMulti1 = New System.Windows.Forms.TextBox()
+        Me.txtSubAnswer = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSub2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtSub1 = New System.Windows.Forms.TextBox()
+        Me.txtAdditionAnswer = New System.Windows.Forms.TextBox()
+        Me.lblAdd = New System.Windows.Forms.Label()
+        Me.txtAddition2 = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtAddition1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtDiviAnswer
+        '
+        Me.txtDiviAnswer.Location = New System.Drawing.Point(203, 206)
+        Me.txtDiviAnswer.Name = "txtDiviAnswer"
+        Me.txtDiviAnswer.Size = New System.Drawing.Size(75, 20)
+        Me.txtDiviAnswer.TabIndex = 45
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(188, 209)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 44
+        Me.Label3.Text = "="
+        '
+        'txtDivi2
+        '
+        Me.txtDivi2.Location = New System.Drawing.Point(108, 206)
+        Me.txtDivi2.Name = "txtDivi2"
+        Me.txtDivi2.Size = New System.Drawing.Size(75, 20)
+        Me.txtDivi2.TabIndex = 43
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(79, 204)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(23, 23)
+        Me.Button4.TabIndex = 42
+        Me.Button4.Text = "/"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'txtDivi1
+        '
+        Me.txtDivi1.Location = New System.Drawing.Point(6, 207)
+        Me.txtDivi1.Name = "txtDivi1"
+        Me.txtDivi1.Size = New System.Drawing.Size(68, 20)
+        Me.txtDivi1.TabIndex = 41
+        '
+        'txtMultiAnswer
+        '
+        Me.txtMultiAnswer.Location = New System.Drawing.Point(203, 168)
+        Me.txtMultiAnswer.Name = "txtMultiAnswer"
+        Me.txtMultiAnswer.Size = New System.Drawing.Size(75, 20)
+        Me.txtMultiAnswer.TabIndex = 40
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(188, 171)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 39
+        Me.Label4.Text = "="
+        '
+        'txtMulti2
+        '
+        Me.txtMulti2.Location = New System.Drawing.Point(108, 168)
+        Me.txtMulti2.Name = "txtMulti2"
+        Me.txtMulti2.Size = New System.Drawing.Size(75, 20)
+        Me.txtMulti2.TabIndex = 38
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(79, 166)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(23, 23)
+        Me.Button5.TabIndex = 37
+        Me.Button5.Text = "x"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'txtMulti1
+        '
+        Me.txtMulti1.Location = New System.Drawing.Point(6, 169)
+        Me.txtMulti1.Name = "txtMulti1"
+        Me.txtMulti1.Size = New System.Drawing.Size(68, 20)
+        Me.txtMulti1.TabIndex = 36
+        '
+        'txtSubAnswer
+        '
+        Me.txtSubAnswer.Location = New System.Drawing.Point(203, 134)
+        Me.txtSubAnswer.Name = "txtSubAnswer"
+        Me.txtSubAnswer.Size = New System.Drawing.Size(75, 20)
+        Me.txtSubAnswer.TabIndex = 35
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(188, 137)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 34
+        Me.Label2.Text = "="
+        '
+        'txtSub2
+        '
+        Me.txtSub2.Location = New System.Drawing.Point(108, 134)
+        Me.txtSub2.Name = "txtSub2"
+        Me.txtSub2.Size = New System.Drawing.Size(75, 20)
+        Me.txtSub2.TabIndex = 33
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(79, 132)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(23, 23)
+        Me.Button2.TabIndex = 32
+        Me.Button2.Text = "-"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtSub1
+        '
+        Me.txtSub1.Location = New System.Drawing.Point(6, 135)
+        Me.txtSub1.Name = "txtSub1"
+        Me.txtSub1.Size = New System.Drawing.Size(68, 20)
+        Me.txtSub1.TabIndex = 31
+        '
+        'txtAdditionAnswer
+        '
+        Me.txtAdditionAnswer.Location = New System.Drawing.Point(203, 96)
+        Me.txtAdditionAnswer.Name = "txtAdditionAnswer"
+        Me.txtAdditionAnswer.Size = New System.Drawing.Size(75, 20)
+        Me.txtAdditionAnswer.TabIndex = 30
+        '
+        'lblAdd
+        '
+        Me.lblAdd.AutoSize = True
+        Me.lblAdd.Location = New System.Drawing.Point(188, 99)
+        Me.lblAdd.Name = "lblAdd"
+        Me.lblAdd.Size = New System.Drawing.Size(13, 13)
+        Me.lblAdd.TabIndex = 29
+        Me.lblAdd.Text = "="
+        '
+        'txtAddition2
+        '
+        Me.txtAddition2.Location = New System.Drawing.Point(108, 96)
+        Me.txtAddition2.Name = "txtAddition2"
+        Me.txtAddition2.Size = New System.Drawing.Size(75, 20)
+        Me.txtAddition2.TabIndex = 28
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(79, 94)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(23, 23)
+        Me.btnAdd.TabIndex = 27
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtAddition1
+        '
+        Me.txtAddition1.Location = New System.Drawing.Point(6, 97)
+        Me.txtAddition1.Name = "txtAddition1"
+        Me.txtAddition1.Size = New System.Drawing.Size(68, 20)
+        Me.txtAddition1.TabIndex = 26
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(63, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(149, 20)
+        Me.Label1.TabIndex = 46
+        Me.Label1.Text = "Simple Calculator"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(62, 78)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 13)
+        Me.Label5.TabIndex = 47
+        Me.Label5.Text = "Enter Button"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(226, 77)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 13)
+        Me.Label6.TabIndex = 48
+        Me.Label6.Text = "Answer"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDiviAnswer)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtDivi2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.txtDivi1)
+        Me.Controls.Add(Me.txtMultiAnswer)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtMulti2)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.txtMulti1)
+        Me.Controls.Add(Me.txtSubAnswer)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSub2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtSub1)
+        Me.Controls.Add(Me.txtAdditionAnswer)
+        Me.Controls.Add(Me.lblAdd)
+        Me.Controls.Add(Me.txtAddition2)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtAddition1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtDiviAnswer As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtDivi2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents txtDivi1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtMultiAnswer As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtMulti2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents txtMulti1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtSubAnswer As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtSub2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents txtSub1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtAdditionAnswer As System.Windows.Forms.TextBox
+    Friend WithEvents lblAdd As System.Windows.Forms.Label
+    Friend WithEvents txtAddition2 As System.Windows.Forms.TextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents txtAddition1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ Simple_Calculator/Simple Calculator.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        Try
+            Dim num1 As Integer = CInt(txtAddition1.Text) 'Convert Integer
+            Dim num2 As Integer = CInt(txtAddition2.Text)
+            Dim sum = num1 + num2
+
+            txtAdditionAnswer.Text = CStr(sum) 'Convert String
+
+        Catch ex As InvalidCastException
+
+            MessageBox.Show("Enter Only Number",
+                            "Error")  'Title in the Message Box
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error",
+                            "Error")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Try
+            Dim num1 As Integer = CInt(txtSub1.Text) 'Convert Integer
+            Dim num2 As Integer = CInt(txtSub2.Text)
+
+            If num1 > num2 Then
+                Dim sum = num1 - num2
+                txtSubAnswer.Text = CStr(sum) 'Convert String
+            Else
+                Dim sum = num2 - num1
+                txtSubAnswer.Text = CStr(sum) 'Convert String
+            End If
+
+
+        Catch ex As InvalidCastException
+
+            MessageBox.Show("Enter Only Number",
+                            "Error")  'Title in the Message Box
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error",
+                            "Error")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Try
+            Dim num1 As Integer = CInt(txtMulti1.Text) 'Convert Integer
+            Dim num2 As Integer = CInt(txtMulti2.Text)
+            Dim sum = num1 * num2
+
+            txtMultiAnswer.Text = CStr(sum) 'Convert String
+
+        Catch ex As InvalidCastException
+
+            MessageBox.Show("Enter Only Number",
+                            "Error")  'Title in the Message Box
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error",
+                            "Error")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Try
+            Dim num1 As Integer = CInt(txtDivi1.Text) 'Convert Integer
+            Dim num2 As Integer = CInt(txtDivi2.Text)
+            Dim sum = num1 / num2
+
+            txtDiviAnswer.Text = CStr(sum) 'Convert String
+
+        Catch ex As InvalidCastException
+
+            MessageBox.Show("Enter Only Number",
+                            "Error")  'Title in the Message Box
+
+        Catch ex As Exception
+
+            MessageBox.Show("Error",
+                            "Error")
+
+        End Try
+
+    End Sub
+End Class
